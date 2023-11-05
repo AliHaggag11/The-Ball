26bf377bfd44cc06469384c9eb8f5b3bc36ad5d2
AliHaggag11 <Ali7aggag@gmail.com>
2023-11-05 16:30:18 +0200

    t2

--- main.js
+++ main.js
@@ -7,11 +7,9 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 const scene = new THREE.Scene()
 
 //Create sphere 
-const geometry = new THREE.TorusGeometry(3, 64, 64)
-const material = new THREE.MeshStandardMaterial({
-  color: "#00ff83",
-  roughness: 0.5
-})
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 ); 
+const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } ); 
+const torus = new THREE.Mesh( geometry, material ); scene.add( torus );
 const mesh = new THREE.Mesh(geometry,material)
 scene.add(mesh)
 
